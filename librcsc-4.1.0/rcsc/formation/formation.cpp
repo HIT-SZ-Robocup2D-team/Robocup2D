741466c10c763c919b1a092d0883af229926f268
LiYikai <1351855206@qq.com>
2020-04-09 19:13:59 +0800

    对formation.cpp注释

--- librcsc-4.1.0/rcsc/formation/formation.cpp
+++ librcsc-4.1.0/rcsc/formation/formation.cpp
@@ -55,7 +55,7 @@ using namespace formation;
 /*!
 
 */
-Formation::Creators &
+Formation::Creators &                 //
 Formation::creators()
 {
     static Creators s_instance;
@@ -66,7 +66,7 @@ Formation::creators()
 /*!
 
 */
-Formation::Ptr
+Formation::Ptr                                                           //根据name将对应的阵型指针赋给ptr
 Formation::create( const std::string & name )
 {
     Formation::Ptr ptr( static_cast< Formation * >( 0 ) );
@@ -124,9 +124,9 @@ Formation::Formation()
 
  */
 void
-Formation::setCenterType( const int unum )
+Formation::setCenterType( const int unum )                 //根据unum（球员号码）给数组赋值确定球员center类型
 {
-    if ( unum < 1 || 11 < unum )
+    if ( unum < 1 || 11 < unum )                            //球员号码只能是1-11
     {
         std::cerr << __FILE__ << ":" << __LINE__
                   << " *** ERROR *** invalid unum " << unum
@@ -134,7 +134,10 @@ Formation::setCenterType( const int unum )
         return;
     }
 
-    M_symmetry_number[unum - 1] = 0;
+    M_symmetry_number[unum - 1] = 0;                  //创建一个数组，通过数组的之来确定球员类型
+                                                      //negative number means this player is original SIDE type.（边位置）
+                                                      //positive numver means that this player is SYMMETRY type and referes other player.
+                                                       //Zero means that this player is CENTER type（中间位置） 
 }
 
 /*-------------------------------------------------------------------*/
@@ -142,7 +145,7 @@ Formation::setCenterType( const int unum )
 
  */
 void
-Formation::setSideType( const int unum )
+Formation::setSideType( const int unum )             //将某个球员（号码传入）设置在球场边的位置
 {
     if ( unum < 1 || 11 < unum )
     {
@@ -160,25 +163,25 @@ Formation::setSideType( const int unum )
 
  */
 bool
-Formation::setSymmetryType( const int unum,
+Formation::setSymmetryType( const int unum,                            //给球员设置symmetryType（意思是给将某个球员的类型设置为和symmetry_unum一样，相当于复制一个，一般用在SIDE位置上对称的左右两个人）
                             const int symmetry_unum,
                             const std::string & role_name )
 {
-    if ( unum < 1 || 11 < unum )
+    if ( unum < 1 || 11 < unum )                                          //判断球员号码
     {
         std::cerr << __FILE__ << ":" << __LINE__
                   << " *** ERROR *** Invalid unum " << unum
                   << std::endl;
         return false;
     }
-    if ( symmetry_unum < 1 || 11 < symmetry_unum )
+    if ( symmetry_unum < 1 || 11 < symmetry_unum )                      //判断symmetry_unum
     {
         std::cerr << __FILE__ << ":" << __LINE__
                   << " *** ERROR *** Invalid symmetry unum " << unum
                   << std::endl;
         return false;
     }
-    if ( symmetry_unum == unum )
+    if ( symmetry_unum == unum )                                        //symmetry_unum不能和unum重合
     {
         std::cerr << __FILE__ << ":" << __LINE__
                   << " *** ERROR *** Never symmetry itself. unum=" << unum
@@ -186,14 +189,14 @@ Formation::setSymmetryType( const int unum,
                   << std::endl;
         return false;
     }
-    if ( M_symmetry_number[symmetry_unum - 1] > 0 )
+    if ( M_symmetry_number[symmetry_unum - 1] > 0 )                     //之前不能被设置过
     {
         std::cerr << __FILE__ << ":" << __LINE__
                   << " *** ERROR *** " << symmetry_unum << " is already a symmetrical player. "
                   << std::endl;
         return false;
     }
-    if ( M_symmetry_number[symmetry_unum - 1] == 0 )
+    if ( M_symmetry_number[symmetry_unum - 1] == 0 )                     //不能是center type
     {
         std::cerr << __FILE__ << ":" << __LINE__
                   << " *** ERROR *** " << symmetry_unum << " is a center type player. "
@@ -203,7 +206,7 @@ Formation::setSymmetryType( const int unum,
 
 
     // check if unum is already assigned as original side type player.
-    for ( int i = 0; i < 11; ++i )
+    for ( int i = 0; i < 11; ++i )                                      ///确定这个要设置的symmetry_unum不能被其他球员占了
     {
         if ( i + 1 == unum ) continue;
         if ( M_symmetry_number[i] == symmetry_unum )
@@ -219,7 +222,7 @@ Formation::setSymmetryType( const int unum,
 
     M_symmetry_number[unum - 1] = symmetry_unum;
 
-    if ( role_name.empty() )
+    if ( role_name.empty() )                                            //设置role
     {
         setRoleName( unum, getRoleName( symmetry_unum ) );
     }
@@ -236,19 +239,19 @@ Formation::setSymmetryType( const int unum,
 
  */
 bool
-Formation::updateRole( const int unum,
+Formation::updateRole( const int unum,                                  //更新role
                        const int symmetry_unum,
                        const std::string & role_name )
 {
-    if ( getSymmetryNumber( unum ) != symmetry_unum )
+    if ( getSymmetryNumber( unum ) != symmetry_unum )                   //当前球员的symmentry_num和
     {
-        if ( symmetry_unum == 0 )
+        if ( symmetry_unum == 0 )                                       //等于0设置为center
         {
             createNewRole( unum, role_name, Formation::CENTER );
             return true;
         }
 
-        if ( symmetry_unum < 0 )
+        if ( symmetry_unum < 0 )                                        //小于0设置为side位置
         {
             createNewRole( unum, role_name, Formation::SIDE );
             return true;
@@ -256,7 +259,7 @@ Formation::updateRole( const int unum,
 
         // ( symmetry_unum > 0 )
 
-        if ( ! isSideType( symmetry_unum ) )
+        if ( ! isSideType( symmetry_unum ) )                            //判断symmetry_unum指向（refer）的球员是否是side类型（<0）
         {
             std::cerr << __FILE__ << ":" << __LINE__
                       << " You cannot use the player number " << symmetry_unum
@@ -265,10 +268,10 @@ Formation::updateRole( const int unum,
             return false;
         }
 
-        for ( int i = 1; i <= 11; ++i )
+        for ( int i = 1; i <= 11; ++i )                                 //遍历找到既不是unum又不是symmetry_unum的球员
         {
             if ( i == unum || i == symmetry_unum ) continue;
-            if ( getSymmetryNumber( i ) == symmetry_unum )
+            if ( getSymmetryNumber( i ) == symmetry_unum )              //该球员的symmetry_unum不能等于symmetry_unum，否则输出错误
             {
                 std::cerr << __FILE__ << ":" << __LINE__
                           << " player number " << symmetry_unum
@@ -278,14 +281,14 @@ Formation::updateRole( const int unum,
             }
         }
 
-        setSymmetryType( unum, symmetry_unum, role_name );
+        setSymmetryType( unum, symmetry_unum, role_name );              //设置symmetry_type
         return true;
     }
 
-    if ( ! role_name.empty()
+    if ( ! role_name.empty()                                            //如果这个球员的role_name为空或者和传入的role_name不等
          && getRoleName( unum ) != role_name )
     {
-        setRoleName( unum, role_name );
+        setRoleName( unum, role_name );                                 //设置
         return true;
     }
 
@@ -297,14 +300,14 @@ Formation::updateRole( const int unum,
 
  */
 bool
-Formation::read( std::istream & is )
+Formation::read( std::istream & is )                                    //读取输入
 {
-    if ( ! readHeader( is ) ) return false;
+    if ( ! readHeader( is ) ) return false;                             //必须Header，Conf，Samples三个都读取到
     if ( ! readConf( is ) ) return false;
     if ( ! readSamples( is ) ) return false;
 
     // check symmetry number circuration reference
-    for ( int i = 0; i < 11; ++i )
+    for ( int i = 0; i < 11; ++i )                                      //检查symmetry_unum有没有大于0
     {
         int refered_unum = M_symmetry_number[i];
         if ( refered_unum <= 0 ) continue;
@@ -329,7 +332,7 @@ Formation::read( std::istream & is )
 
  */
 std::ostream &
-Formation::print( std::ostream & os ) const
+Formation::print( std::ostream & os ) const                             //输出Header、Conf、Samples
 {
     if ( os ) printHeader( os );
     if ( os ) printConf( os );
@@ -347,20 +350,20 @@ Formation::readHeader( std::istream & is )
 {
     std::string line_buf;
 
-    while ( std::getline( is, line_buf ) )
+    while ( std::getline( is, line_buf ) )                              //不断读取getline
     {
         if ( line_buf.empty()
              || line_buf[0] == '#'
-             || ! line_buf.compare( 0, 2, "//" ) )
+             || ! line_buf.compare( 0, 2, "//" ) )                      //？？？
         {
             continue;
         }
 
-        std::istringstream istr( line_buf );
+        std::istringstream istr( line_buf );                            //从流中提取数据，将缓冲区定义出来istr
 
-        std::string tag;
+        std::string tag;                                                //string定义类为tag
         istr >> tag;
-        if ( tag != "Formation" )
+        if ( tag != "Formation" )                                       //如果tag不是"Formation"
         {
             std::cerr << __FILE__ << ":" << __LINE__
                       << " *** ERROR *** Found invalid tag ["
@@ -369,9 +372,9 @@ Formation::readHeader( std::istream & is )
             return false;
         }
 
-        std::string name;
+        std::string name;                                               //定义string类name
         istr >> name;
-        if ( name != methodName() )
+        if ( name != methodName() )                                     //如果name不等于methodame，也就是不匹配
         {
             std::cerr << __FILE__ << ":" << __LINE__
                       << " *** ERROR *** Unsupported formation type name "
@@ -382,7 +385,7 @@ Formation::readHeader( std::istream & is )
         }
 
         int ver = 0;
-        if ( istr >> ver )
+        if ( istr >> ver )                                              //读取ersion并判断赋值
         {
             if ( ver < 0 )
             {
@@ -395,7 +398,7 @@ Formation::readHeader( std::istream & is )
 
             M_version = ver;
         }
-        else
+        else                                                            //没读取到就直接设为0
         {
             M_version = 0;
         }
@@ -411,7 +414,7 @@ Formation::readHeader( std::istream & is )
 
  */
 bool
-Formation::readSamples( std::istream & is )
+Formation::readSamples( std::istream & is )                             //读取Samples
 {
     M_samples = SampleDataSet::Ptr( new SampleDataSet() );
 
@@ -429,7 +432,7 @@ Formation::readSamples( std::istream & is )
 
  */
 std::ostream &
-Formation::printHeader( std::ostream & os ) const
+Formation::printHeader( std::ostream & os ) const                       //打印Header
 {
     os << "Formation " << methodName() << ' ' << version() << '\n';
     return os;
@@ -440,9 +443,9 @@ Formation::printHeader( std::ostream & os ) const
 
  */
 std::ostream &
-Formation::printSamples( std::ostream & os ) const
+Formation::printSamples( std::ostream & os ) const                      //打印Header
 {
-    if ( M_samples )
+    if ( M_samples )                                                    //只在sample>0才打印
     {
         M_samples->print( os );
     }
